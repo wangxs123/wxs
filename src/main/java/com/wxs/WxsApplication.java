ad18988b9df63cf52d365848a2be386b5fcc2998
wxs <wxs_zzu@163.com>
2019-05-28 20:24:49 +0800

    11

---
+++ src/main/java/com/wxs/WxsApplication.java
@@ -0,0 +1,17 @@
+package com.wxs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wxs
+ * @date
+ */
+@SpringBootApplication
+public class WxsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxsApplication.class, args);
+    }
+
+}
