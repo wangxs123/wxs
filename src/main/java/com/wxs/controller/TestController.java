07e6a93d889e307c051e2aba9456a94e512acc2f
wxs <wxs_zzu@163.com>
2019-07-24 11:50:23 +0800

    基础配置

--- src/main/java/com/wxs/controller/TestController.java
+++ src/main/java/com/wxs/controller/TestController.java
@@ -1,8 +1,13 @@
 package com.wxs.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import common.entity.UserEntity;
+import common.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author wxs
@@ -11,9 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class TestController {
+    @Reference(version = "0.0.1")
+    UserService userService;
 
     @GetMapping("/getHello")
     public String getHello(){
         return "hello Dubbo .....";
+    }
+    @GetMapping("/getUserInfo")
+    public List<UserEntity> getUserInfo(){
+        List<UserEntity> userList = userService.getAllUserInfo();
+        return userList;
     }
 }
